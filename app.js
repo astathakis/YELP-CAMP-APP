1195460255bfe40a73ec1e67d9a0e84377c78695
tasos <tasos_stathakis@yahoo.com>
2022-12-07 21:21:03 +0200

    basic CRUD done

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-// mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true);
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   //mongoose 6 no longer support useNewUrlParser,useCreateIndex,useUnifiedTopology
@@ -22,6 +23,11 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+//enable to parse the body of the request
+app.use(express.urlencoded({ extented: true }));
+//method-override
+app.use(methodOverride('_method'));
+
 //+++++++++++++Set up our REST routes+++++++++++
 
 app.get('/', (req, res) => {
@@ -31,6 +37,44 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
+});
+
+// +++create new camp has two route handlers++++++
+// one to serve our form
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+// and one for the post
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+// ++++++++++++show route+++++++++++
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
+// +++++++++++edit route++++++++++
+//get the id and prepopulated the form with the information
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+});
+//faking post request from form with a put request using method-override
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// +++++++++++++++delete route+++++++++
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
 });
 
 // +++++++++++listen on port 3000++++++++++++
